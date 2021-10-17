f7b88625056251fc08309f9a760de5d5effcd95f
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-10-17 16:48:00 -0300

    feat: add component loading

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,11 @@
+import loading from './loading.module.css'
+const Loading = () => {
+  return (
+    <div className={loading.ripple}>
+        <div></div>
+        <div></div>
+    </div>
+  );
+}
+
+export default Loading;
